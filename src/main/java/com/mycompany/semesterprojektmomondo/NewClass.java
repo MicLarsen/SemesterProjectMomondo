636084384c7889fab485882da86c1b28eba91f43
nickl <nickl@10.50.131.30>
2016-11-21 12:03:59 +0100

    Github link added

--- src/main/java/com/mycompany/semesterprojektmomondo/NewClass.java
+++ src/main/java/com/mycompany/semesterprojektmomondo/NewClass.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.semesterprojektmomondo;
 
 /**
@@ -10,5 +5,11 @@ package com.mycompany.semesterprojektmomondo;
  * @author nicolaicornelis
  */
 public class NewClass {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    
     
 }
