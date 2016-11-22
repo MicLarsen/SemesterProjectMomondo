8e4428ca4d6ec16b71cf8d81ff9aac104782a67e
nickl <nickl@10.50.131.30>
2016-11-22 13:02:43 +0100

    json dependency

--- src/main/java/interfaces/S2SInterface.java
+++ src/main/java/interfaces/S2SInterface.java
@@ -1,7 +1,7 @@
 package interfaces;
 
 import java.util.List;
-import javax.json.Json;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -9,9 +9,9 @@ import javax.json.Json;
  */
 public interface S2SInterface {
     //Refactor with url parameter for multiple choices of airlines
-    public Json getJsonFromServer(String from, String date, int tickets);
+    public JSONObject getJsonFromServer(String from, String date, int tickets);
     
-    public Json getJsonFromServer(String from, String to, String date, int tickets);
+    public JSONObject getJsonFromServer(String from, String to, String date, int tickets);
     
     //Persons should contain the personal information required to book the tickets!
     public String bookTickets(String flightId, int tickets, List<String> persons);
